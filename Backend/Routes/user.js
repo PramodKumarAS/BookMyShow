d78f58a7475c94de483c15c1dd12bf29cdd0100e
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-25 21:36:22 +0530

    Added bcrypt for storing passwords

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,18 +1,48 @@
 const express=require("express");
 const userModel = require("../Model/users")
-const router =  express.Router();
+const userRouter =  express.Router();
+const bcrypt = require("bcrypt");
 
-router.post('/login',async (req,res)=>{
+userRouter.post('/register',async(req,res)=>{
+    try {
+        const userDetail = req.body;
+        const hashedPassword =await hashPassword(userDetail.password);
+
+        const user =  new userModel(userDetail);
+        user.password = hashedPassword;
+        await user.save();
+
+        res.status(200).json({
+            success:true,
+            message:"Registration is successfull",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success:false, 
+            message:"Internal server error",
+        })        
+    }
+});
+
+userRouter.post('/login',async (req,res)=>{
 
     try {       
         const userDetail = req.body;
     
         const user = await userModel.findOne({email:userDetail.email});
-    
+        
         if(!user){
             return res.status(404).json({ success: false, message: "No user found" });
         }
-    
+        
+        const isPasswordValid =await verifyPassword(req.body.password,user.password);
+        if (!isPasswordValid) {
+            return res.status(404).send({ 
+                success: false, 
+                message: "No user/pass combo found" 
+            });
+        }
+
         res.status(200).json({
             success:true,
             message: "Successfully logged in!",
@@ -25,4 +55,13 @@ router.post('/login',async (req,res)=>{
     }
 });
 
-module.exports=router;
+async function hashPassword(password) {
+    const saltRounds=10;
+    return await bcrypt.hash(password,saltRounds);
+}
+
+async function verifyPassword(password,hash) {
+    return await bcrypt.compare(password,hash)
+}
+
+module.exports=userRouter;
