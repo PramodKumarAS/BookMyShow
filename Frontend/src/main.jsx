8cab764708b2ca0d31e72a2b031667fe9d260142
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-26 21:25:27 +0530

    Deploy try-6

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { App as AntdApp} from "antd"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { App as AntdApp } from "antd";
+import { BrowserRouter } from 'react-router-dom'; // ✅ add this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AntdApp>
-      <App />
-    </AntdApp>
-  </React.StrictMode>,
-)
+    <BrowserRouter> {/* ✅ wrap for routing */}
+      <AntdApp>
+        <App />
+      </AntdApp>
+    </BrowserRouter>
+  </React.StrictMode>
+);
