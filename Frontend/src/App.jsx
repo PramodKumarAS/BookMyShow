92296c71a1510b5f5fd51cf1110120cadfe2066e
Pramod Kumar <pramodkumaras143@gmail.com>
2025-07-26 21:33:53 +0530

    Deploy try -7

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
 import User from './Pages/User'
 
 function App() {
-
   return (
-    <>
-     <BrowserRouter>
-        <Routes>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-            <Route path='/user' element={<User/>}></Route>
-        </Routes>
-     </BrowserRouter>
-    </>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/user' element={<User />} />
+    </Routes>
   )
 }
 
